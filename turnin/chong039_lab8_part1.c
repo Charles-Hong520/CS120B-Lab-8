8c719a6ae7c68e3bf8c7cddf5d8f62aaffefc408
Charles-Hong520 <56359884+Charles-Hong520@users.noreply.github.com>
2021-10-30 11:04:36 -0700

    Update chong039_lab8_part1.c

--- turnin/chong039_lab8_part1.c
+++ turnin/chong039_lab8_part1.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *  
- *  Demo Link: 
+ *  Demo Link: https://drive.google.com/file/d/1HwZXhw1DattA-h8JiJOIlFS4OPqCQu2_/view?usp=sharing
  */
 #include <avr/io.h>
 
